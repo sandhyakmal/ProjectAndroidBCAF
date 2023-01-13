92aaa13ed5d26e7417f1bcee057ac8d83dc2fff4
Jett <jericochandra4@gmail.com>
2023-01-13 09:13:26 +0700

    Commit layout

--- app/src/main/java/com/bcafinance/myapplication/TelfonActivity.kt
+++ app/src/main/java/com/bcafinance/myapplication/TelfonActivity.kt
@@ -14,27 +14,30 @@ import kotlinx.android.synthetic.main.activity_telfon.*
         setContentView(R.layout.activity_telfon)
 
             teleponYa.setOnClickListener({
-                txtPrfx.visibility = View.GONE
-                cardPrefix.visibility = View.GONE
-                txtTlp1.visibility = View.GONE
-                cardTlp1.visibility = View.GONE
+                inpTelepon1Update.isEnabled = true
+//                txtPrfx.visibility = View.GONE
+//                cardPrefix.visibility = View.GONE
+//                txtTlp1.visibility = View.GONE
+//                cardTlp1.visibility = View.GONE
             })
 
             telepon2Ya.setOnClickListener {
-                txtVwTelpon2.visibility = View.GONE
-                cardTelfon2.visibility = View.GONE
+                inpTelepon2Update.isEnabled = true
+//                txtVwTelpon2.visibility = View.GONE
+//                cardTelfon2.visibility = View.GONE
             }
 
             telefonTidak.setOnClickListener({
-                txtPrfx.visibility = View.VISIBLE
-                cardPrefix.visibility = View.VISIBLE
-                txtTlp1.visibility = View.VISIBLE
-                cardTlp1.visibility = View.VISIBLE
+                inpTelepon1Update.isEnabled = false
+//                cardPrefix.visibility = View.VISIBLE
+//                txtTlp1.visibility = View.VISIBLE
+//                cardTlp1.visibility = View.VISIBLE
             })
 
             telefon2Tidak.setOnClickListener {
-                txtVwTelpon2.visibility = View.VISIBLE
-                cardTelfon2.visibility = View.VISIBLE
+                inpTelepon1Update.isEnabled = false
+//                txtVwTelpon2.visibility = View.VISIBLE
+//                cardTelfon2.visibility = View.VISIBLE
             }
 
 
